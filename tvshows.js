3550b042eaef3ed5ef036fa7ca1de6facb47a5e2
javub25 <90850225+javub25@users.noreply.github.com>
2022-09-14 12:44:58 +0200

    Llamada getYear

--- tvshows.js
+++ tvshows.js
@@ -1,7 +1,15 @@
-import {Menu, evento_btnAnterior, evento_btnSiguiente, getYear} from './functions.js';
-Menu();
-getYear();
-evento_btnAnterior();
-evento_btnSiguiente();
+import {getYear, checkYear, requestTVShows, evento_btnAnterior, evento_btnSiguiente} from './functions.js';
+
+getYear(Year_User => {
+    /*En caso de que el año sea correcto y aparte tenga un valor, cargamos las series con el año escogido.
+    */
+    if(checkYear(Year_User) && Year_User!==undefined)
+    {
+        requestTVShows(Year_User);
+        evento_btnAnterior(Year_User);
+        evento_btnSiguiente(Year_User);
+    }
+    else alert("Año incorrecto");
+})
 
 
