6874964936c7b26dc0b833c8b80cfaab84ce2ae4
javub25 <90850225+javub25@users.noreply.github.com>
2022-09-14 12:43:09 +0200

    Añadir getYear y modificar nombres de funciones

--- functions.js
+++ functions.js
@@ -1,8 +1,6 @@
-"use strict";
-
 let page = 1;
 
-
+//Evento que cargará la anterior página del catálogo de series
 export let evento_btnAnterior = (Year) =>
 {
     const btnAnterior = document.querySelector("#btnAnterior");
@@ -11,11 +9,11 @@ export let evento_btnAnterior = (Year) =>
         if(page > 1)
         {
             page--;
-            cargarSeries(Year);
+            requestTVShows(Year);
         }
-   
     })
 }
+//Evento que cargará la siguiente página del catálogo de series
 export let evento_btnSiguiente = (Year) =>
 {
     const btnSiguiente = document.querySelector("#btnSiguiente");
@@ -24,60 +22,67 @@ export let evento_btnSiguiente = (Year) =>
         if(page < 1000)
         {
             page++;
-            cargarSeries(Year);
+            requestTVShows(Year);
         }
     })
 }
-
-export const Menu = () =>
-{
-    const menu = document.querySelector(".menu");
-    const menu_apartados = document.createElement("div");
-    const desplegable = document.querySelector(".material-symbols-outlined");
-        
-    //Mostramos el menú cuando se clique en el icono del despleable
-    desplegable.addEventListener("click", () =>
-    {
-        menu_apartados.innerHTML = `
-        <div class="menu_apartados">
-            <div>
-                <div>
-                    <span class="material-symbols-outlined">
-                    close
-                    </span>
-                </div>
-            </div>
-
-            <div>
-                <a href="films.html" style="text-decoration:none; color:white">
-                    <p>
-			            MOVIES
-                    </p>
-                </a>
-		    </div>
-
-            <div>
-                <a href="tvshows.html" style="text-decoration:none; color:white">
-                    <p>
-                        TV SHOWS
-                    </p>
-                </a>
-		    </div>
-        </div>
-        `;
-        menu.appendChild(menu_apartados);
-
-        const cerrar = document.getElementsByClassName("menu_apartados")[0];
-
-        cerrar.addEventListener("click", () =>
-        {
-            menu.removeChild(menu_apartados);
-        });
+//Obtenemos el año que el usuario escriba a partir del evento
+export const getYear = Year_User => {
+    const button = document.querySelector(".button");
+    button.addEventListener("click", () => {
+        const Formulario = document.querySelector(".FormFecha");
+        const Year = new FormData(Formulario).get("Year");
+        Year_User(Year);
     });
-}
-
+  }
+  //Mostramos el menú
+  export const Menu = () =>
+  {
+      const menu = document.querySelector(".menu");
+      const menu_apartados = document.createElement("div");
+      const desplegable = document.querySelector(".material-symbols-outlined");
+          
+      //Mostramos el menú cuando se clique en el icono del desplegable
+      desplegable.addEventListener("click", () =>
+      {
+          menu_apartados.innerHTML = `
+          <div class="menu_apartados">
+              <div>
+                  <div>
+                      <span class="material-symbols-outlined">
+                      close
+                      </span>
+                  </div>
+              </div>
+              <div>
+                  <a href="films.html" style="text-decoration:none; color:white">
+                      <p>
+                          MOVIES
+                      </p>
+                  </a>
+              </div>
+              <div>
+                  <a href="tvshows.html" style="text-decoration:none; color:white">
+                      <p>
+                          TV SHOWS
+                      </p>
+                  </a>
+              </div>
+          </div>
+          `;
+          menu.appendChild(menu_apartados);
+  
+          const cerrar = document.getElementsByClassName("menu_apartados")[0];
+  
+          cerrar.addEventListener("click", () =>
+          {
+              menu.removeChild(menu_apartados);
+          });
+      });
+  }
+  
 //Comprovamos que el año sea correcto
-const checkYear = (Year) =>
+export const checkYear = (Year) =>
 {
     //Expresion que comprueba que el año este entre 1900 y 2080
     let expr = new RegExp(/^(19|20)[\d]{2,2}$/);
@@ -85,23 +90,22 @@ const checkYear = (Year) =>
     return expr.test(Year);
 }
 
-export async function cargarSeries(Year) 
+export async function requestTVShows(Year) 
 {
     try
     { 
-        if(checkYear(Year) === true)
-        {
-            //La API nos devolverá las series del año que eliga el usuario ordenadas de menor a mayor
-            const respuesta = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=91e2cd3a9a469c7556f0539d4e755dc3&first_air_date_year=${Year}&sort_by=first_air_date.desc&page=${page}&language=es-ESP`);
-            if(respuesta.status === 200)
+        //La API nos devolverá las series del año que eliga el usuario ordenadas de menor a mayor
+        const respuesta = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=91e2cd3a9a469c7556f0539d4e755dc3&first_air_date_year=${Year}&sort_by=first_air_date.desc&page=${page}&language=es-ESP`);
+        if(respuesta.status === 200)
         {
             let datos = respuesta.data;
             const contenedor = document.querySelector(".contenedor");
 
             let serie = '';
             datos.results.map((series) =>
-                {
-                    let getYear = series.first_air_date.slice(0,4);
+            {
+                //Obtenemos los últimos 4 digitos del año de cada serie, para mostrar únicamente los del año que ponga el usuario
+                let getYear = series.first_air_date.slice(0,4);
 
                     if(getYear === Year)
                     {
@@ -113,7 +117,6 @@ export async function cargarSeries(Year)
                                 <div>
                                     <img src=https://image.tmdb.org/t/p/w500/${series.poster_path} alt="" class="imagenPeli"/>
                                 </div>
-
                                 <div class="textoPeli">
                                     <h3>
                                         ${series.name}
@@ -130,11 +133,10 @@ export async function cargarSeries(Year)
                             </div>   
                         `
                         contenedor.innerHTML = serie;
-
-                        if(series.poster_path === null)
-                        {
-                            throw new Error(`La imagen no fue añadida en ${series.name}`);
-                        }  
+                            if(series.poster_path === null)
+                            {
+                                throw new Error(`La imagen no fue añadida en ${series.name}`);
+                            }  
                         }
                         catch(error)
                         {
@@ -143,61 +145,13 @@ export async function cargarSeries(Year)
                     }      
                 }
             )
-            }
-            else if(respuesta.status === 404) 
-                throw new Error("La pelicula no se encuentra");
-        
-            else if(respuesta.status === 401) 
-                throw new Error("Servidor no disponible");
-
-            else 
-                throw new Error("Error de Busqueda");
         }
-        //Mostraremos un mensaje de error cuando no se haya escrito un año correcto
-        else
-        {
-            alert("Año incorrecto");
-        }
+        else if(respuesta.status === 404) throw new Error("La pelicula no se encuentra");
+        else if(respuesta.status === 401) throw new Error("Servidor no disponible");
+        else throw new Error("Error de Busqueda");
     }
     catch(error)
     {
         console.error(error.message);
     }
 }
-
-export const getYear = () =>
-{
-    const button = document.querySelector(".button");
-
-    button.addEventListener("click", () =>
-    {
-        const Formulario = document.querySelector(".FormFecha");
-        const Year = new FormData(Formulario).get("Year");
-        cargarSeries(Year);
-        evento_btnAnterior(Year);
-        evento_btnSiguiente(Year);
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
