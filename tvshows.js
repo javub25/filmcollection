83fb073165e3ec85e0bacb8ce41b680efe38efeb
Javi <javuberay@outlook.com>
2022-09-25 19:59:26 +0200

    Add getGenres

--- tvshows.js
+++ tvshows.js
@@ -1,15 +1,22 @@
-import {getYear, checkYear, requestTVShows, evento_btnAnterior, evento_btnSiguiente} from './functions.js';
+import {Menu, getYear, getGenre, checkYear, requestTVShows, evento_btnAnterior, evento_btnSiguiente} from './functions.js';
 
-getYear(Year_User => {
+Menu();
+
+getYear((Year_User) => {
     /*En caso de que el año sea correcto y aparte tenga un valor, cargamos las series con el año escogido.
     */
     if(checkYear(Year_User) && Year_User!==undefined)
     {
-        requestTVShows(Year_User);
-        evento_btnAnterior(Year_User);
-        evento_btnSiguiente(Year_User);
+        //Obtendremos el genero cuando haya sido verificado el año
+        getGenre((GenreId) => 
+        {
+            requestTVShows(Year_User, GenreId);
+            evento_btnAnterior(Year_User, GenreId);
+            evento_btnSiguiente(Year_User, GenreId);
+        })
     }
-    else alert("Año incorrecto");
-})
-
-
+    else
+    {
+        alert("Año incorrecto");
+    }
+});
